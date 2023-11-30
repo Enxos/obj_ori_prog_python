77638bf49d9d0a378b33d21e971e3ca6a8df7054
Enxos <23334313+Enxos@users.noreply.github.com>
2023-11-30 17:18:53 +0100

    4

--- fcomho.py
+++ fcomho.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import datetime
+
 from pprint import pprint
 
 class Szoba():
@@ -42,17 +44,31 @@ class KetagyasSzoba(Szoba):
     pass
 
 class Foglalas():
-    def __init__(self, datum):
+    def __init__(self, datum, utyfel, szobaszam):
+
+        if not isinstance(datum, datetime.date):
+            raise Exception('Ez bizony nem datum batyus!')
         self._datum = datum
+        self._utyfel = utyfel
+        self._szobaszam = szobaszam
         pass
 
     @property
     def datum(self):
         return self._datum
+
+    @property
+    def utyfel(self):
+        return self._utyfel
+
+    @property
+    def szobaszam(self):
+        return self._szobaszam
     pass
 
 class Szalloda():
     _szobak = []
+    _foglalasok = []
     def __init__(self, nev, db_egy=5, db_ketto=7):
         self._nev = nev
 
@@ -66,12 +82,43 @@ class Szalloda():
             self._szobak.append(KetagyasSzoba(szam))
             pass
 
-        pprint(self._szobak)
+        #pprint(self._szobak)
         pass
 
     @property
     def nev(self):
         return nev
+
+    def foglal(self, datum, szobaszam, kicsoda):
+        if not isinstance(datum, datetime.date):
+            raise Exception('Ez bizony nem datum batyus!')
+
+        if datum <= datetime.date.today():
+            raise Exception('A mult nem a jovendo')
+
+        szoba = None
+        for sz in self._szobak:
+            if sz.szam == szobaszam:
+                szoba = sz
+                break
+            pass
+        if szoba is None:
+            raise Exception('Szoba {sz} not found'.format(sz = szobaszam))
+
+        for f in self._foglalasok:
+            if f.szobaszam == szoba.szam and datum == f.datum:
+                raise Exception('E mar foglalt e!')
+            pass
+
+        self._foglalasok.append(Foglalas(datum = datum,
+                                         utyfel = kicsoda,
+                                         szobaszam = szobaszam))
+        return sz.ar
+
     pass
 
 s = Szalloda('foobar')
+x = s.foglal(datetime.date(2023, 12, 13), 1, 'en')
+pprint(x)
+x = s.foglal(datetime.date(2023, 12, 14), 11, 'te')
+pprint(x)
